d150c6e51ef898949a3f9036abccf080ff4e3ae8
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-01-26 01:25:36 +0530

    Update About.jsx
    Fixed an issue

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,37 +14,37 @@ export const About = () => {
             <ul className={styles.aboutItems}>
                 <li className={styles.aboutItem}>
                     <img src={getImageUrl("about/cursor.png")} alt="Cursor Icon" />
+                    <div className={styles.aboutItemText}>
+                        <h3>Frontend Developer</h3>
+                        <p>
+                            I'm a frontend developer with experience in building 
+                            responsive and optimized websites.
+                        </p>
+                    </div>
                 </li>
-                <div className={styles.aboutItemText}>
-                    <h3>Frontend Developer</h3>
-                    <p>
-                        I'm a frontend developer with experience in building 
-                        responsive and optimized websites.
-                    </p>
-                </div>
-
+                
                 <li className={styles.aboutItem}>
                     <img src={getImageUrl("about/cpu.png")} alt="CPU Icon" />
+                    <div className={styles.aboutItemText}>
+                        <h3>Backend Developer</h3>
+                        <p>
+                            I have experience developing fast and optimized 
+                            back-end systems & APIs.
+                        </p>
+                    </div>
                 </li>
-                <div className={styles.aboutItemText}>
-                    <h3>Backend Developer</h3>
-                    <p>
-                        I have experience developing fast and optimized 
-                        back-end systems & APIs.
-                    </p>
-                </div>
 
 
                 <li className={styles.aboutItem}>
                     <img src={getImageUrl("about/desktop.png")} alt="Desktop Icon" />
+                    <div className={styles.aboutItemText}>
+                        <h3>UI UX Designer</h3>
+                        <p>
+                            I have designed multiple landing pages and have 
+                            created design system as well using Figma.
+                        </p>
+                    </div>
                 </li>
-                <div className={styles.aboutItemText}>
-                    <h3>UI UX Designer</h3>
-                    <p>
-                        I have designed multiple landing pages and have 
-                        created design system as well using Figma.
-                    </p>
-                </div>
             </ul>
         </div>
     </section>
