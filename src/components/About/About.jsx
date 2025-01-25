d1fb14c1e38f6164142b991fd8823dccfd74344f
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-01-26 01:01:18 +0530

    Update About.jsx
    Completed about.jsx without css

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import styles from './About.module.css';
+import {getImageUrl} from '../../utils';
 
 export const About = () => {
   return (
-    <section>
-        <h2>About</h2>
+    <section className={styles.container}>
+        <h2 className={styles.title}>About</h2>
+        <div className={styles.content}>
+            <img src={getImageUrl("about/aboutImage.png")} 
+                alt="Me sitting and working with my laptop" 
+                className={styles.aboutImage}
+            />
+            <ul className={styles.aboutItems}>
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl("about/cursor.png")} alt="Cursor Icon" />
+                </li>
+                <div className={styles.aboutItemText}>
+                    <h3>Frontend Developer</h3>
+                    <p>
+                        I'm a frontend developer with experience in building 
+                        responsive and optimized websites.
+                    </p>
+                </div>
+
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl("about/cpu.png")} alt="CPU Icon" />
+                </li>
+                <div className={styles.aboutItemText}>
+                    <h3>Backend Developer</h3>
+                    <p>
+                        I have experience developing fast and optimized 
+                        back-end systems & APIs.
+                    </p>
+                </div>
+
+
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl("about/desktop.png")} alt="Desktop Icon" />
+                </li>
+                <div className={styles.aboutItemText}>
+                    <h3>UI UX Designer</h3>
+                    <p>
+                        I have designed multiple landing pages and have 
+                        created design system as well using Figma.
+                    </p>
+                </div>
+            </ul>
+        </div>
     </section>
   )
 }
