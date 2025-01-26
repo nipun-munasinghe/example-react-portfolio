61960d851d23fac3cbfa8c45449e2665fac87333
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-01-26 23:01:38 +0530

    Add css and did some changes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,8 +6,10 @@ export const Contact = () => {
   return (
     <footer id='contact' className={styles.container}>
         <div className={styles.text}>
-            <h2>Contact</h2>
-            <p>Feel free to reach out!</p>
+            <div>
+                <h2>Contact</h2>
+                <p>Feel free to reach out!</p>
+            </div>
 
             <ul className={styles.links}>
                 <li className={styles.link}>
