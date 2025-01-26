38d05d84f763283e3f6425759516acf338261be1
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-01-26 11:31:02 +0530

    Set environment for Experience part

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import styles from "./App.module.css";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Hero } from "./components/Hero/Hero";
 import { About } from "./components/About/About";
+import { Experience } from "./components/Experience/Experience";
 
 function App() {
 
@@ -10,8 +11,9 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Experience />
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Experience.module.css';
+import {getImageUrl} from '../../utils';
+
+export const Experience = () => {
+  return (
+    <div>Experience</div>
+  )
+}
