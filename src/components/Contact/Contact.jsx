98b976941486616ec2ccec25a0cf23f4c4ea8ec2
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-01-26 22:33:08 +0530

    Change image icon names
    Change image icon names & finished contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,6 +4,34 @@ import { getImageUrl } from "../../utils";
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <footer id='contact' className={styles.container}>
+        <div className={styles.text}>
+            <h2>Contact</h2>
+            <p>Feel free to reach out!</p>
+
+            <ul className={styles.links}>
+                <li className={styles.link}>
+                    <img src={getImageUrl("contact/email.png")} 
+                         alt="Email Icon" 
+                    />
+                    <a href="mailto:neha@gmail.com">neha@gmail.com</a>
+                </li>
+
+                <li className={styles.link}>
+                    <img src={getImageUrl("contact/linkedin.png")} 
+                         alt="LinkedIn Icon" 
+                    />
+                    <a href="https://www.linkedin.com/neha">linkedin.com/neha</a>
+                </li>
+
+                <li className={styles.link}>
+                    <img src={getImageUrl("contact/github.png")} 
+                         alt="GitHub Icon" 
+                    />
+                    <a href="https://www.github.com/neha-neha">github.com/neha-neha</a>
+                </li>
+            </ul>
+        </div>
+    </footer>
   )
 }
