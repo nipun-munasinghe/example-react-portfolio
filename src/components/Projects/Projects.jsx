d4a6b957b8155ddb858714e63ad018620db41214
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-01-26 16:23:49 +0530

    Set environment for creating Projects part

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from "./Projects.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Projects = () => {
+  return (
+    <div>Projects</div>
+  )
+}
