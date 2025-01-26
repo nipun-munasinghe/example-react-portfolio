0ca7b6857f6266e69d8480df63e5ee400b86f17d
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-01-26 22:07:06 +0530

    Set environment for creating Contact part

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
