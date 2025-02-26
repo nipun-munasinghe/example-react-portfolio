93ecf6fc53f999840b060851ef11522fee30d4e2
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-02-26 21:57:37 +0530

    Updated vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/example-react-portfolio/', // Replace with your GitHub repository name
   css: {
     modules: {
       localsConvention: 'camelCase',
     },
   },
-})
+});
